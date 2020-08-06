b5f07b9f290a144e5a0660e3a8527e339254402e
Michail Michailidis <mmichaelid@gmail.com>
2020-08-06 10:43:57 +0300

    white list swagger so it works on http://localhost:5000/api/v1/swagger-ui.html

--- src/main/java/com/delivery/presenter/config/WebSecurityConfig.java
+++ src/main/java/com/delivery/presenter/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -59,6 +60,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .userDetailsService(customUserDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(AUTH_WHITELIST);
+    }
+
+
+    private static final String[] AUTH_WHITELIST = {
+            "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**"
+    };
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
